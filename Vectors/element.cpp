428f2fdd9aac9e71354b93977059eea085225102
kris326 <kris_kk326@abv.bg>
2020-05-18 14:18:14 +0300

    Element class

---
+++ Vectors/element.cpp
@@ -0,0 +1,50 @@
+#include "element.h"
+using namespace std;
+
+Element::Element()
+{
+}
+
+Element::~Element()
+{
+}
+
+double Element::getX() const
+{
+	return 0.0;
+}
+
+double Element::getY() const
+{
+	return 0.0;
+}
+
+double Element::getZ() const
+{
+	return 0.0;
+}
+
+void Element::print() const
+{
+}
+
+istream& Element::inserter(istream& i)
+{
+	return i;
+}
+
+ostream& Element::extractor(ostream& o) const
+{
+	return o;
+}
+
+istream& operator>>(istream& i, Element& el)
+{
+	return el.inserter(i);
+}
+
+ostream& operator<<(ostream& o, const Element& el)
+{
+	return el.extractor(o);
+}
+ 
--- Vectors/element.h
+++ Vectors/element.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
+class Element {
+public:
+	Element();
+	~Element();
+	virtual double getX() const;
+	virtual double getY() const;
+	virtual double getZ() const;
+	virtual void print() const;
+	virtual istream& inserter(istream& i);
+	virtual ostream& extractor(ostream& o) const;
+private:
+
+};
+
+istream& operator>>(istream& i, Element& el);
+ostream& operator<<(ostream& o, const Element& el);
