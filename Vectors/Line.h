047c86939157430e74e2c82981ab18608e0c5329
Shad0w <teodor.lozanov@gmail.com>
2020-06-03 16:53:29 +0300

    started building Line class

--- Vectors/Line.h
+++ Vectors/Line.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "Vector.h"
+#include <iostream>
+
+class Line : public Vector {
+public :
+	//constructores
+	Line();
+	Line(Point&, Vector&);
+	Line(Point&, Point&);
+	~Line();
+
+	//functions
+	virtual void VectorDirection(const Line&) const;
+
+	//overload operators
+
+private:
+	Point x, y;
+	Vector vector;
+};
