c7a35c7fa383d65f03500b27048e89bba5cb6a67
hehehexdd <everydaystuff42069@gmail.com>
2020-06-07 23:40:53 +0300

    main menu

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -10,19 +10,56 @@
 using namespace std;
 
 void Testing() {
-	Point a(2, 2, 2);
-    Point b(5, 6, 7);
-    Point c(19.3, 15, 20.3);
-    //Triangle t1;
-	//cin>>t1;
-    Triangle t2(a, b, c);
-    Triangle t3;
-    t3 = t2;
+	char ch;
+	Element * e;
+	do{
+		list();
+		cin>>ch;
+	}while(ch<'0' || ch>'6');
 
-	//cout<<"\nt1: "<<t1<<"\nt2: "<<t2<<"\nt3: "<<t3<<endl;
-	cout<<"t3 area: "<< t3.area() << endl;
-	cout<<"t3 per: "<< t3.perimeter() << endl;
-	cout<<"t3 centroid: "<< t3.centroid() << endl;
+	switch(ch) {
+		case '1':
+			system("cls");
+			e = new Point();
+			cin>>*e;
+			e->menu();
+			break;
+		case '2':
+			system("cls");
+			e = new Vector();
+			cin>>e;
+			ch = 'y'
+			while(ch == 'y' || ch == 'Y') {
+				system("cls");
+				//cout<< *e<<endl;
+				e->menu();
+				cout<<"\nWould you like to continue? (y/n) ";
+				cin >> ch;
+			}
+			break;
+		case '3':
+			system("cls");
+		case '4':
+			system("cls");
+		case '5':
+			system("cls");
+			e = new Triangle();
+			cin >> *e;
+			c = 'y';
+			while (c == 'y' || c == 'Y') {
+				system("cls");
+				cout << *e << endl;
+				a->menu();
+				cout<<"\nWould you like to continue? (y/n) ";
+				cin >> ch;
+			}
+			break;
+		case '6':
+			system("cls");
+		case '0':
+			system("exit");
+	}
+}
 	
 
 
@@ -34,4 +71,15 @@ int main() {
 	Testing();
 
 	return 0;
+}
+
+void list() {
+	cout<<"Project: Vectors\n\n";
+	cout<<"1. Point\n";
+	cout<<"2. Vector\n";
+	cout<<"3. Triangle\n";
+	cout<<"4. Line\n";
+	cout<<"5. Segment\n";
+	cout<<"6. Tetrahedron\n";
+	cout<<"0. Exit\n";
 }
