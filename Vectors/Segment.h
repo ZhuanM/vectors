a0ae04c8df9ec6b96b259d959c07ce20bc32d4d7
Shad0w <teodor.lozanov@gmail.com>
2020-06-08 02:13:28 +0300

    segment operator ==

--- Vectors/Segment.h
+++ Vectors/Segment.h
@@ -1,30 +1,30 @@
-// #pragma once
-// #include "Line.h"
-// #include <iostream>
+ #pragma once
+ #include "Line.h"
+ #include <iostream>
 
-// class Segment : public Line {
+ class Segment : public Line {
 
-// public:
-//     Segment();
-//     //Segment(const Line&);
-//     Segment(const Point&, const Vector&);
-//     Segment(const Point&, const Vector&, double, double);
-//     ~Segment();
-//     Segment(const Segment&);
-//     Segment& operator=(const Segment&);
+ public:
+     Segment();
+     //Segment(const Line&);
+     Segment(const Point&, const Vector&);
+     Segment(const Point&, const Vector&, double, double);
+     ~Segment();
+     Segment(const Segment&);
+     Segment& operator=(const Segment&);
 
-//     double getT(int n)const;
-//     void setT(int, double);
-//     double length();
-//     Point midPoint(); 
+     double getT(int n)const;
+     void setT(int, double);
+     double length();
+     Point midPoint(); 
 
-//     bool operator==(const Point&);
+     bool operator==(const Point&);
     
-//     //virtual void print() const;
+     //virtual void print() const;
 
-//     virtual std::istream& extractor(std::istream& i);
-//     virtual std::ostream& inserter(std::ostream& o) const;
+     virtual std::istream& extractor(std::istream& i);
+     virtual std::ostream& inserter(std::ostream& o) const;
 
-// private:
-//     double t[2];
-// };
+ private:
+     double t[2];
+ };
